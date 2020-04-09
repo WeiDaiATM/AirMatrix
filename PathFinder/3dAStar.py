0bf5bd856d1eae11a7f5699dd8c0c33028af8b8e
wdai mac <daiwei@tju.edu.cn>
2020-04-09 14:10:27 +0800

    initial finish: a-star algorithm

--- PathFinder/3dAStar.py
+++ PathFinder/3dAStar.py
@@ -4,15 +4,11 @@ Created on Wed Apr  1 11:43:17 2020
 
 @author: WeiDai
 
-A Star algorithm with time cost
+A-Star algorithm with time cost
 time is computed from distance and aircraft speed.
 
-startPoint: (x,y)
-desitnationPoint: (x,y)
-network: matrixLink
-aircraftspeed: (horizontal speed, vertical speed, speed with 45 degrees angle, speed with 35 degrees angle)
 """
-import pandas as np
+# import pandas as np
 
 class Point:
     def __init__(self,x,y,z):
@@ -26,32 +22,41 @@ class Point:
         return "x:"+str(self.x)+",y:"+str(self.y)+",z:"+str(self.z)
                        
     
-class Node:  # 描述AStar算法中的节点数据
-    def __init__(self, point, endPoint, g=0):
+class Node:
+    def __init__(self, point, endPoint, indexRange, g=0):
         self.point = point  # coordinate
-        self.g = g  
-        self.h = (abs(endPoint[0] - point[0]) + abs(endPoint[1] - point[1]) + abs(endPoint[2] - point[2]))  # manhattan distance
+        self.g = g
+        self.h = (abs(endPoint[0] - point[0]) + abs(endPoint[1] - point[1]) + abs(endPoint[2] - point[2]))
+        # self.hIndex = (abs(endPoint[0] - point[0]),abs(endPoint[1] - point[1]),abs(endPoint[2] - point[2]))  # manhattan distance
         self.index = int(point[0]+point[1]*indexRange[0]+point[2]*indexRange[0]*indexRange[1])
+        self.father = None
         
 class Aircraft:
-    def __init__(self, ):
+    def __init__(self, speed):
+        """
+        :param speed: (horizontal, vertical, 45degree, 35degree)
+        """
+        self.speed = speed
         
         
 class AStar:
     # 3-d a star algorithm
-    def __init__(self, matrix, startPoint, endPoint):
+    def __init__(self, matrix, startPoint, endPoint, aircraft):
         """
         :param matrix: pre-generated matrix object
         :param startPoint: (x,y,z)
         :param endPoint: (x,y,z)
+        :param aircraft: aircraft object
         """
         self.openList = []
         self.closeList = []
         self.matrix = matrix
         self.startPoint = startPoint
         self.endPoint = endPoint
+        self.matrix = matrix
+        self.aircraft = aircraft
     
-    def getMinNode(self):
+    def GetMinNode(self):
         """
         get the node with min F value in openList
         :return: Node
@@ -62,7 +67,7 @@ class AStar:
                 currentNode = node
         return currentNode
     
-    def pointInCloseList(self, point):
+    def PointInCloseList(self, point):
         """
         see if node is in open list
         """
@@ -71,33 +76,60 @@ class AStar:
                 return True
         return False
  
-    def pointInOpenList(self, point):
+    def PointInOpenList(self, point):
         for node in self.openList:
             if node.point == point:
                 return node
         return None
     
-    def endPointInCloseList(self):
+    def EndPointInCloseList(self):
         for node in self.openList:
             if node.point == self.endPoint:
                 return node
         return None
     
-    def search():
+    def Search(self):
         # add startPoint to openList
-        startNode = Node(self.startPoint, self.endPoint)
+        startNode = Node(self.startPoint, self.endPoint, self.matrix.indexRange)
         self.openList.append(startNode)
         # search
         while True:
             # find minF
-            minF = self.getMinNode()
+            minF = self.GetMinNode()
             
             # minF to closeList
             self.closeList.append(minF)
             self.openList.remove(minF)
             
             # test the neighbour of minF
-            
+            neighbours = self.matrix.network[minF.index]
+            for next in neighbours:
+                currentPoint = Point(self.matrix.nodeList[next[0]]["index"])
+                if self.PointInCloseList(currentPoint):# ignore if in close list
+                    continue
+                nextNode = Node(currentPoint, self.endPoint, self.matrix.indexRange)
+                # nextNode.h = nextNode.hIndex[0]*self.matrix.cellLength + nextNode.hIndex[1]*self.matrix.cellWidth + nextNode.hIndex[2]*self.matrix.cellHeight
+                nextNode.g = minF.g + next[1]/self.aircraft.speed[next[2]]
+
+                existNode = self.PointInOpenList(currentPoint) # return if the node exist in open list
+                if not existNode:
+                    nextNode.father = minF
+                    self.openList.append(nextNode)
+                    continue
+                if nextNode.g < existNode.g: # set g and father if the node exist in open list
+                    existNode.g = nextNode.g
+                    existNode.father = minF
+            lastNode = self.EndPointInCloseList()
+            if lastNode:# if endPoint is in close list, return result
+                pathList = []
+                while True:
+                    if lastNode.father:
+                        pathList.append(lastNode.point)
+                        lastNode = lastNode.father
+                    else:
+                        return list(reversed(pathList))
+            if len(self.openList)==0:
+                return None
 
 
 
--- matrix/matrix.py
+++ matrix/matrix.py
@@ -8,21 +8,19 @@ Created on Wed Apr  8 10:25:14 2020
 Matrix Building class
 
 """
-class point:
-    def __init__():
-        
-        
+import numpy as np
+
 class matrix:
     
     def __init__(self, matrixRange, cellSize=(100,100,50)):
         self.matrixRange =  matrixRange
         self.cellLength, self.cellWidth, self.cellHeight = cellSize
-        self.horizontalDist = np.sqrt(cellLength*cellLength+cellWidth*cellWidth)
-        self.verticalDist = np.sqrt(cellLength*cellLength+cellHeight*cellHeight) # assuming width = length
-        self.diagonalDist = np.sqrt(cellLength*cellLength+cellWidth*cellWidth+
-                       cellHeight*cellHeight)
-        self.indexRange = (int(matrixRange[0]/cellLength+1), int(matrixRange[1]/cellWidth+1), 
-              int(matrixRange[2]/cellHeight+1))
+        self.horizontalDist = np.sqrt(self.cellLength*self.cellLength+self.cellWidth*self.cellWidth)
+        self.verticalDist = np.sqrt(self.cellLength*self.cellLength+self.cellHeight*self.cellHeight) # assuming width = length
+        self.diagonalDist = np.sqrt(self.cellLength*self.cellLength+self.cellWidth*self.cellWidth+
+                       self.cellHeight*self.cellHeight)
+        self.indexRange = (int(matrixRange[0]/self.cellLength+1), int(matrixRange[1]/self.cellWidth+1),
+              int(matrixRange[2]/self.cellHeight+1))
         
         self.nodeList = list()
         
@@ -31,7 +29,7 @@ class matrix:
             for y in range(indexRange[1]):
                 for x in range(indexRange[0]):
                     self.nodeList.append(dict(index=(x,y,z),coordinate=
-                                          (x*cellLength,y*cellWidth,z*cellHeight)))
+                                          (x*self.cellLength,y*self.cellWidth,z*self.cellHeight)))
                     
         self.network = list()
         for i in range(len(self.nodeList)):
@@ -57,82 +55,82 @@ class matrix:
             # 3 refers to 35 degree
             
             if lx>=0:
-                link.append((int(lx+y*indexRange[0]+z*indexRange[0]*indexRange[1]),cellLength, 0))
+                link.append((int(lx+y*indexRange[0]+z*indexRange[0]*indexRange[1]), self.cellLength, 0))
                 if ly>=0:
-                    link.append((int(lx+ly*indexRange[0]+z*indexRange[0]*indexRange[1]),horizontalDist, 0))
+                    link.append((int(lx+ly*indexRange[0]+z*indexRange[0]*indexRange[1]), self.horizontalDist, 0))
                     if lz>=0:
-                        link.append((int(lx+ly*indexRange[0]+lz*indexRange[0]*indexRange[1]),diagonalDist, 3))
+                        link.append((int(lx+ly*indexRange[0]+lz*indexRange[0]*indexRange[1]),  self.diagonalDist, 3))
                     if hz<=indexRange[2]:
-                        link.append((int(lx+ly*indexRange[0]+hz*indexRange[0]*indexRange[1]),diagonalDist, 3))
+                        link.append((int(lx+ly*indexRange[0]+hz*indexRange[0]*indexRange[1]), self.diagonalDist, 3))
                 
                 if lz>=0:
-                    link.append((int(lx+y*indexRange[0]+lz*indexRange[0]*indexRange[1]),verticalDist, 2))
+                    link.append((int(lx+y*indexRange[0]+lz*indexRange[0]*indexRange[1]), self.verticalDist, 2))
                 if hz<=indexRange[2]:
-                    link.append((int(lx+y*indexRange[0]+hz*indexRange[0]*indexRange[1]),verticalDist, 2))
+                    link.append((int(lx+y*indexRange[0]+hz*indexRange[0]*indexRange[1]), self.verticalDist, 2))
                 if hy<=indexRange[1]:
-                    link.append((int(lx+hy*indexRange[0]+z*indexRange[0]*indexRange[1]),horizontalDist, 0))
+                    link.append((int(lx+hy*indexRange[0]+z*indexRange[0]*indexRange[1]),self.horizontalDist, 0))
                     if hz<=indexRange[2]:
-                        link.append((int(lx+hy*indexRange[0]+hz*indexRange[0]*indexRange[1]),diagonalDist, 3))
+                        link.append((int(lx+hy*indexRange[0]+hz*indexRange[0]*indexRange[1]), self.diagonalDist, 3))
                     if lz>=0:
-                        link.append((int(lx+hy*indexRange[0]+lz*indexRange[0]*indexRange[1]),diagonalDist, 3))
+                        link.append((int(lx+hy*indexRange[0]+lz*indexRange[0]*indexRange[1]), self.diagonalDist, 3))
             
             if ly>=0:
-                link.append((int(x+ly*indexRange[0]+z*indexRange[0]*indexRange[1]),cellWidth, 0))
+                link.append((int(x+ly*indexRange[0]+z*indexRange[0]*indexRange[1]),self.cellWidth, 0))
                 if hz<=indexRange[2]:
-                    link.append((int(x+ly*indexRange[0]+hz*indexRange[0]*indexRange[1]),verticalDist, 2))
+                    link.append((int(x+ly*indexRange[0]+hz*indexRange[0]*indexRange[1]), self.verticalDist, 2))
                 if lz>=0:
-                    link.append((int(x+ly*indexRange[0]+lz*indexRange[0]*indexRange[1]),verticalDist, 2))
+                    link.append((int(x+ly*indexRange[0]+lz*indexRange[0]*indexRange[1]), self.verticalDist, 2))
             if lz>=0:
-                link.append((int(x+y*indexRange[0]+lz*indexRange[0]*indexRange[1]),cellHeight, 1))
+                link.append((int(x+y*indexRange[0]+lz*indexRange[0]*indexRange[1]),self.cellHeight, 1))
             if hz<=indexRange[2]:
-                link.append((int(x+y*indexRange[0]+hz*indexRange[0]*indexRange[1]),cellHeight, 1))
+                link.append((int(x+y*indexRange[0]+hz*indexRange[0]*indexRange[1]),self.cellHeight, 1))
                 if hy<=indexRange[1]:
-                    link.append((int(x+hy*indexRange[0]+hz*indexRange[0]*indexRange[1]),verticalDist, 2))
+                    link.append((int(x+hy*indexRange[0]+hz*indexRange[0]*indexRange[1]), self.verticalDist, 2))
             if hy<=indexRange[1]:
-                link.append((int(x+hy*indexRange[0]+z*indexRange[0]*indexRange[1]),cellWidth, 0))
+                link.append((int(x+hy*indexRange[0]+z*indexRange[0]*indexRange[1]),self.cellWidth, 0))
                 if lz>=0:
-                    link.append((int(x+hy*indexRange[0]+lz*indexRange[0]*indexRange[1]),verticalDist, 2))
+                    link.append((int(x+hy*indexRange[0]+lz*indexRange[0]*indexRange[1]), self.verticalDist, 2))
             
             if hx <=indexRange[0]:
-                link.append((int(hx+y*indexRange[0]+z*indexRange[0]*indexRange[1]),cellLength, 0))
+                link.append((int(hx+y*indexRange[0]+z*indexRange[0]*indexRange[1]),self.cellLength, 0))
                 if lz>=0:
-                    link.append((int(hx+y*indexRange[0]+lz*indexRange[0]*indexRange[1]),verticalDist, 2))
+                    link.append((int(hx+y*indexRange[0]+lz*indexRange[0]*indexRange[1]), self.verticalDist, 2))
                 if hz<=indexRange[2]:
-                    link.append((int(hx+y*indexRange[0]+hz*indexRange[0]*indexRange[1]),verticalDist, 2))
+                    link.append((int(hx+y*indexRange[0]+hz*indexRange[0]*indexRange[1]), self.verticalDist, 2))
                 
                 if ly>=0:
-                    link.append((int(hx+ly*indexRange[0]+z*indexRange[0]*indexRange[1]),horizontalDist, 0))
+                    link.append((int(hx+ly*indexRange[0]+z*indexRange[0]*indexRange[1]),self.horizontalDist, 0))
                     if lz>=0:
-                        link.append((int(hx+ly*indexRange[0]+lz*indexRange[0]*indexRange[1]),diagonalDist, 3))
+                        link.append((int(hx+ly*indexRange[0]+lz*indexRange[0]*indexRange[1]), self.diagonalDist, 3))
                     if hz<=indexRange[2]:
-                        link.append((int(hx+ly*indexRange[0]+hz*indexRange[0]*indexRange[1]),diagonalDist, 3))
+                        link.append((int(hx+ly*indexRange[0]+hz*indexRange[0]*indexRange[1]), self.diagonalDist, 3))
                 
                 if hy<=indexRange[1]:
-                    link.append((int(hx+hy*indexRange[0]+z*indexRange[0]*indexRange[1]),horizontalDist, 0))
+                    link.append((int(hx+hy*indexRange[0]+z*indexRange[0]*indexRange[1]),self.horizontalDist, 0))
                     if lz>=0:
-                        link.append((int(hx+hy*indexRange[0]+lz*indexRange[0]*indexRange[1]),diagonalDist, 3))
+                        link.append((int(hx+hy*indexRange[0]+lz*indexRange[0]*indexRange[1]), self.diagonalDist, 3))
                     if hz<=indexRange[2]:
-                        link.append((int(hx+hy*indexRange[0]+hz*indexRange[0]*indexRange[1]),diagonalDist, 3))
+                        link.append((int(hx+hy*indexRange[0]+hz*indexRange[0]*indexRange[1]), self.diagonalDist, 3))
                         
             if lx>=0:
-                link.append((int(lx+y*indexRange[0]+z*indexRange[0]*indexRange[1]),cellLength, 0))
+                link.append((int(lx+y*indexRange[0]+z*indexRange[0]*indexRange[1]),self.cellLength, 0))
                 if ly>=0:
-                    link.append((int(lx+ly*indexRange[0]+z*indexRange[0]*indexRange[1]),horizontalDist, 0))
+                    link.append((int(lx+ly*indexRange[0]+z*indexRange[0]*indexRange[1]),self.horizontalDist, 0))
                     if lz>=0:
-                        link.append((int(lx+ly*indexRange[0]+lz*indexRange[0]*indexRange[1]),diagonalDist, 3))
+                        link.append((int(lx+ly*indexRange[0]+lz*indexRange[0]*indexRange[1]), self.diagonalDist, 3))
                     if hz<=indexRange[2]:
-                        link.append((int(lx+ly*indexRange[0]+hz*indexRange[0]*indexRange[1]),diagonalDist, 3))
+                        link.append((int(lx+ly*indexRange[0]+hz*indexRange[0]*indexRange[1]), self.diagonalDist, 3))
                 
                 if lz>=0:
-                    link.append((int(lx+y*indexRange[0]+lz*indexRange[0]*indexRange[1]),verticalDist, 2))
+                    link.append((int(lx+y*indexRange[0]+lz*indexRange[0]*indexRange[1]), self.verticalDist, 2))
                 if hz<=indexRange[2]:
-                    link.append((int(lx+y*indexRange[0]+hz*indexRange[0]*indexRange[1]),verticalDist, 2))
+                    link.append((int(lx+y*indexRange[0]+hz*indexRange[0]*indexRange[1]), self.verticalDist, 2))
                 if hy<=indexRange[1]:
-                    link.append((int(lx+hy*indexRange[0]+z*indexRange[0]*indexRange[1]),horizontalDist, 0))
+                    link.append((int(lx+hy*indexRange[0]+z*indexRange[0]*indexRange[1]),self.horizontalDist, 0))
                     if hz<=indexRange[2]:
-                        link.append((int(lx+hy*indexRange[0]+hz*indexRange[0]*indexRange[1]),diagonalDist, 3))
+                        link.append((int(lx+hy*indexRange[0]+hz*indexRange[0]*indexRange[1]), self.diagonalDist, 3))
                     if lz>=0:
-                        link.append((int(lx+hy*indexRange[0]+lz*indexRange[0]*indexRange[1]),diagonalDist, 3))
+                        link.append((int(lx+hy*indexRange[0]+lz*indexRange[0]*indexRange[1]), self.diagonalDist, 3))
             self.network.append(link)
             
             
